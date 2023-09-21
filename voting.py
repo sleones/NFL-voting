c320422426c8056093c69a10211f65b93acfe2c3
Shawn Leones <shawncleones@gmail.com>
2023-09-21 19:08:14 -0400

    Initial commit

---
+++ voting.py
@@ -0,0 +1,17 @@
+import pyautogui
+import time
+
+i=0
+time.sleep(3)
+while i<1:
+	
+	pyautogui.click(1744,511)
+	time.sleep(1)
+	pyautogui.click(1768,826)
+	time.sleep(.5)
+	pyautogui.click(1920,505)
+	time.sleep(2.5)
+	pyautogui.click(1912,691)
+	time.sleep(.5)
+	pyautogui.click(381,489)
+	time.sleep(.5)
