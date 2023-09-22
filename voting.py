960cb35da1822ded5916241e07c9d11e79b6cc20
Shawn Leones <shawncleones@gmail.com>
2023-09-22 10:24:44 -0400

    Added Functions

--- voting.py
+++ voting.py
@@ -1,45 +1,29 @@
-import time
 import pyautogui
 import opencv
 
-i=0
-j=0
-l=0
-k=0
-m=0
-n=0
+buttons = ['Player.png','Vote.png','Submit.png','Again.png','Counter.png']
 
-time.sleep(3)
-while i<3:
-    while j<1:
-        if pyautogui.locateCenterOnScreen('Nacua.png', confidence = .95, region = (1280, 0 , 1280, 720)) is not None:
-            x,y = pyautogui.locateCenterOnScreen('Nacua.png', confidence = .95, region = (1280, 0 , 1280, 720))
+#def networkError(): Refresh page to clear error
+
+#def screenShot(): Take screenshots of buttons for user
+
+def selectPlayer(player):
+    buttons[0] = player + '.png' #this doesn't account for other image formats
+    
+def locateOnScreen():
+    i = 0
+    while i < len(buttons): 
+        if pyautogui.locateCenterOnScreen(buttons[i], confidence = .95) is not None:
+            x,y = pyautogui.locateCenterOnScreen(buttons[i], confidence = .95)
             pyautogui.click(x,y)
-            j+=1
-            n=0
-    while k<1:
-        if pyautogui.locateCenterOnScreen('Vote.png',confidence = .9) is not None:
-            x,y = pyautogui.locateCenterOnScreen('Vote.png', confidence = .9)
-            pyautogui.click(x,y)
-            k+=1
-            j-=1
-    while l<1:
-        if pyautogui.locateCenterOnScreen('Submit.png', confidence = .9) is not None:
-            x,y = pyautogui.locateCenterOnScreen('Submit.png', confidence = .9)
-            pyautogui.click(x,y)
-            l+=1
-            k-=1
-    while m<1:
-        if pyautogui.locateCenterOnScreen('Again.png', confidence = .9) is not None:
-            x,y = pyautogui.locateCenterOnScreen('Again.png', confidence = .9)
-            pyautogui.click(x,y)
-            m+=1
-            l-=1
-    while n<1:
-        if pyautogui.locateCenterOnScreen('Counter.png', confidence = .9, region = (0, 0, 1280, 720)) is not None:
-            x,y = pyautogui.locateCenterOnScreen('Counter.png', confidence = .9, region = (0, 0, 1280, 720))
-            pyautogui.click(x,y)
-            m-=1
-            n+=1
-            
-   
+            i += 1
+
+votes = 0
+print('please enter player\'s last name to vote for.')
+player = input()
+selectPlayer(player)
+while(True):
+    locateOnScreen()
+    votes +=1
+    print('votes =',votes)
+    
